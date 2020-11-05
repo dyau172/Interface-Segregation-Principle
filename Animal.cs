58e0956288ffa38eb0d0e15ee240d4ce74c9f4d3
dyau172 <47676379+dyau172@users.noreply.github.com>
2020-11-05 11:24:12 +0000

    Added Eat method

--- Animal.cs
+++ Animal.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Interface_Segregation_Principle {
     class Animal {
+        public void Eat(){
+            Console.WriteLine("Nom nom");
+        }
         public void Walk () {
             Console.WriteLine ("Walking..");
         }
