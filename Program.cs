960140f0b71802a78a3b338312b2cb48320ed8b8
dyau172 <47676379+dyau172@users.noreply.github.com>
2020-11-07 13:39:51 +0000

    Create cat object

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 
-namespace Interface_Segregation_Principle
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Dog dog1 = new Dog();
-            dog1.Bark();
+namespace Interface_Segregation_Principle {
+    class Program {
+        static void Main (string[] args) {
+            Dog spot = new Dog ();
+            spot.Bark ();
+
+            Cat garfield = new Cat();
+            garfield.Meow();
+
+
         }
     }
 }
