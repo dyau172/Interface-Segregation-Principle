6eafb49b7d0490557b3085c6c5176d8caa24b25a
Viktorija Garifova <kairotv21@gmail.com>
2020-11-08 15:07:06 +0000

    Cat class layout fixing

--- Cat.cs
+++ Cat.cs
@@ -1,18 +1,19 @@
 using System;
 
-namespace Interface_Segregation_Principle {
-    class Cat : ICat, ISleep {
+namespace Interface_Segregation_Principle 
+{
+    class Cat : ICat, ISleep 
+    {
         public string Name { get; set; }
 
-        public void Sleep(){
+        public void Sleep()
+        {
             Console.WriteLine("Zzzzz");
         }
 
-
-        public void Meow () {
+        public void Meow () 
+        {
             Console.WriteLine ("Meeeeeow");
         }
-
     }
-
 }
