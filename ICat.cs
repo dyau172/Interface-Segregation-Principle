aecf07905fdf9711b416457aeaa8e377bbafe9af
Viktorija Garifova <kairotv21@gmail.com>
2020-11-08 16:04:39 +0000

    Added new systems

--- ICat.cs
+++ ICat.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Interface_Segregation_Principle
 {
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Interface_Segregation_Principle 
 {
