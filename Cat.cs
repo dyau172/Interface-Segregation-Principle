666fd98c9184d96908988fb0af6c8b6934b72b9b
Viktorija Garifova <kairotv21@gmail.com>
2020-11-08 15:31:48 +0000

    Added new method to class cat

--- Cat.cs
+++ Cat.cs
@@ -6,14 +6,24 @@ namespace Interface_Segregation_Principle
     {
         public string Name { get; set; }
 
-        public void Sleep()
-        {
-            Console.WriteLine("Zzzzz");
-        }
-
         public void Meow() 
         {
             Console.WriteLine ("Meeeeeow");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Num...Num...Num...");
+        }
+
+        public void Sleep(int time)
+        {
+            Console.WriteLine("Zzzzz...");
+        }
+
+        public void Walk(int Yaxis, int Xaxis)
+        {
+            Console.WriteLine("Stamp...stamp...stamp...");
         }
     }
 }
