fc991fff626defaf48ac37e88b19572d0f1d9637
Viktorija Garifova <kairotv21@gmail.com>
2020-11-08 15:46:36 +0000

    Added new methods to class dolphine

--- Dolphine.cs
+++ Dolphine.cs
@@ -1,1 +1,24 @@
+using System;
 
+namespace Interface_Segregation_Principle
+{
+    class Dog : IDog
+    {
+        public string Name { get; set; }
+
+        public void Eat()
+        {
+            Console.WriteLine("Num...Num...Num...");
+        }
+
+        public void Sleep(int time)
+        {
+            Console.WriteLine("Zzzzz...");
+        }
+
+        public void Swiming(int Yaxis, int Xaxis, int Zaxis)
+        {
+            Console.WriteLine("Plop...plop...plop...");
+        }
+    }
+}
